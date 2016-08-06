65ede78b9c67367d312a2c7f0516f22329cc90be
Brian Bouterse <bmbouter@gmail.com>
2016-08-06 12:23:43 -0400

    Fixes queue length charts bug
    The previous charts were totally wrong.

--- queue_length_charts.py
+++ queue_length_charts.py
@@ -5,26 +5,29 @@ import matplotlib.pyplot as plt
 import numpy
 
 
-# R_VALUES_INCREASING_PATH = '~/Documents/Research/matlab/R_values_increasing/'
+R_VALUES_INCREASING_PATH = '~/Documents/Research/matlab/OLD_R_values_increasing/'
 # ERROR_PATH = '~/Documents/Research/matlab/sim_vs_numerical_software__error_investigation/'
-# TRAFFIC_INTENSITY_PATH = '~/Documents/Research/matlab/increasing_traffic_intensity/'
+TRAFFIC_INTENSITY_PATH = '~/Documents/Research/matlab/OLD_increasing_traffic_intensity/'
 
 SIM_VARY_INTENSITY_01 = '~/Documents/Research/matlab/80_percent_violation_sim_only/vary_R_intensity_01/'
 SIM_VARY_INTENSITY_05 = '~/Documents/Research/matlab/80_percent_violation_sim_only/vary_R_intensity_05/'
 SIM_VARY_INTENSITY_10 = '~/Documents/Research/matlab/80_percent_violation_sim_only/vary_R_intensity_10/'
 SIM_VARY_R = '~/Documents/Research/matlab/80_percent_violation_sim_only/vary_traffic_intensity/'
 
+OLD_80_PERCENT = '~/Documents/Research/matlab/OLD_80_percent_violation_vs_traffic_intensity/'
 
-DATA_DIR = os.path.expanduser(SIM_VARY_INTENSITY_01)
+
+DATA_DIR = os.path.expanduser(OLD_80_PERCENT)
 
 
 def get_queue_length_data():
     pattern_match = re.compile('queue_length_distribution.*({.*})')
+    # pattern_match = re.compile('queue_depth probabilities.*(\[.*\])')
     queue_length_data = {}
     for root, sub_folders, filenames in os.walk(DATA_DIR):
         for filename in filenames:
-            # if filename == 'results.txt':
-            if filename == 'sim_output.txt':
+            if filename == 'results.txt':
+            # if filename == 'sim_output.txt':
                 test_name = root.split(os.sep)[-1]
                 full_path = os.path.join(root, filename)
                 with open(full_path, 'r') as file_handle:
@@ -43,14 +46,25 @@ if __name__ == "__main__":
     mean_data = []
     percentile_99_data = []
     for key in sorted(data.keys()):
-        values_dict = data[key]
-        values_list = [values_dict[i] for i in sorted(values_dict.keys())]
-        percentile_99_data.append(numpy.percentile(values_list, 99))
-        mean_data.append(numpy.mean(values_list))
+        queue_length_pdf = data[key]
+        if isinstance(queue_length_pdf, dict):
+            the_real_distribution = []
+            for depth in queue_length_pdf.keys():
+                depth_probability = queue_length_pdf[depth]
+                the_real_distribution.append(depth_probability * depth)
+            queue_length_pdf = the_real_distribution
+        mean_data.append(sum(queue_length_pdf))
+
+        queue_length_cdf = []
+        accumulator = 0
+        for queue_depth in range(len(queue_length_pdf)):
+            accumulator += queue_length_pdf[queue_depth]
+            queue_length_cdf.append(accumulator * queue_depth)
+        percentile_99_data.append(numpy.percentile(queue_length_cdf, 99))
         x_values.append(re.findall(r'\d+', key)[0])
     chart_path = DATA_DIR + 'queue_depth.png'
     plt.plot(x_values, mean_data, label='Mean')
     plt.plot(x_values, percentile_99_data, label='99th Percentile')
-    plt.legend(loc='upper right', shadow=True, fontsize='x-large')
+    plt.legend(loc='best', shadow=True, fontsize='x-large')
     plt.savefig(chart_path)
     plt.close()
